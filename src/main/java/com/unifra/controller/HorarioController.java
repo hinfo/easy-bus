09b72f6bb93373da80d0803c102642addfdf2b9d
Henrique Antunes <hinfo@gmx.com>
2017-10-27 18:37:44 -0200

    Adicionado novas linhas para testes.

--- src/main/java/com/unifra/controller/HorarioController.java
+++ src/main/java/com/unifra/controller/HorarioController.java
@@ -29,10 +29,34 @@ public class HorarioController {
     @ResponseBody
     public List<Linha> getHorariosByEmpresa(@RequestParam("id") String empresaId) {
         List<Linha> horarios = new ArrayList<>();
-        Linha linha = new Linha();
-        linha.setHora("10:20");
-        linha.setSentido("Centro-Bairro");
-        horarios.add(linha);
+        Linha linha1 = new Linha();
+        linha1.setHora("10:20");
+        linha1.setSentido("Centro-Bairro");
+        horarios.add(linha1);
+        Linha linha2 = new Linha();
+        linha2.setHora("10:30");
+        linha2.setSentido("Centro-Bairro");
+        horarios.add(linha2);
+        Linha linha3 = new Linha();
+        linha3.setHora("10:40");
+        linha3.setSentido("Centro-Bairro");
+        horarios.add(linha3);
+        Linha linha4 = new Linha();
+        linha4.setHora("10:50");
+        linha4.setSentido("Centro-Bairro");
+        horarios.add(linha4);
+        Linha linha5 = new Linha();
+        linha5.setHora("11:00");
+        linha5.setSentido("Centro-Bairro");
+        horarios.add(linha5);
+        Linha linha6 = new Linha();
+        linha6.setHora("11:10");
+        linha6.setSentido("Centro-Bairro");
+        horarios.add(linha6);
+        Linha linha7 = new Linha();
+        linha7.setHora("11:20");
+        linha7.setSentido("Centro-Bairro");
+        horarios.add(linha7);
 
         return horarios;
     }
